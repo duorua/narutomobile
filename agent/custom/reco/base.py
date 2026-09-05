68702195d5474dd36f5d12b6e8ed21994b7b9905
duorua <103261312+duorua@users.noreply.github.com>
2026-09-06 01:09:24 +0800

    docs:完善man-MaaFwApp不尝试唤起qq描述

--- agent/custom/reco/base.py
+++ agent/custom/reco/base.py
@@ -513,11 +513,11 @@ class CheckIsAndroid(CustomRecognition):
             {
                 "start_up_switch_social_media_account": {
                     "action": "DoNothing",
-                    "focus": {"Node.Action.Starting": '<font color="tomato">安卓端man不能尝试唤起qq</font>'},
+                    "focus": {"Node.Action.Starting": '<font color="tomato">安卓端man-MaaFwApp目前不尝试唤起qq</font>'},
                 },
                 "switch_account_switch_social_media_account": {
                     "action": "DoNothing",
-                    "focus": {"Node.Action.Starting": '<font color="tomato">安卓端man不能尝试唤起qq</font>'},
+                    "focus": {"Node.Action.Starting": '<font color="tomato">安卓端man-MaaFwApp目前不尝试唤起qq</font>'},
                 },
             },
         )
